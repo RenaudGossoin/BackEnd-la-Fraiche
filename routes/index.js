46c0fda5a316ccd6a678209bc922d4457ee96312
Amour Benlaoubi <a.benlaoubi@gmail.com>
2022-03-09 17:37:37 +0100

    test ep

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,11 @@ var LockerModel = require("../models/lockers");
 var OrderModel = require("../models/orders");
 var UserModel = require("../models/users");
 
+
+
 var uid2 = require("uid2");
 var bcrypt = require("bcrypt");
+/*test*/
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
@@ -204,5 +207,8 @@ router.get("/account", async function (req, res, next) {
 router.get("/detail-product", async function (req, res, next) {
   res.json({ result });
 });
+
+
+
 
 module.exports = router;
