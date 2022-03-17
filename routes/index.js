85803f3dc148990d836752050687a09dea57c04f
Elodie <phauv.elodie@gmail.com>
2022-03-17 17:19:17 +0100

    commentaires des routes

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,18 @@ var UserModel = require("../models/users");
 
 var uid2 = require("uid2");
 var bcrypt = require("bcrypt");
-/*test*/
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-/*sign-up screen*/
+/*sign-up screen
+On reçoit depuis le frontend plusieurs infos : username, email, le password et le departement
+1ere etape: recherche des éventuelles erreurs à pusher dans le tableau error
+2e etape: si aucune erreur, on enregistre cet user dans la bdd grâce au modèle user. 
+On recupère les informations du front et on passe tout en caractères minuscules, on protège le mdp grâce à la fonction hashage bcrypt
+On génère un token unique à l'uilisateur qui permettra de l'identifier ultérieurement
+On renvoie au front en réponse les infos user, result, error, et le token */
 router.post("/sign-up", async function (req, res, next) {
   var error = [];
   var result = false;
@@ -60,7 +65,12 @@ router.post("/sign-up", async function (req, res, next) {
   res.json({ result, saveUser, error, token });
 });
 
-/*sign-in screen*/
+/*sign-in screen
+On reçoit depuis le frontend l'email et le mdp.
+1er etape: on verifie les erreurs
+2e etape: si aucune erreur, on recherche l'user en bdd grâce à l'email
+Si l'user existe, on compare le mdp saisi en front avec celui en bdd grâce à la fonction bcrypt.compareSync. S'il y a correspondance, on attribue à la variable token, le token de l'user en bdd sinon erreur mdp
+Si l'user n'existe pas, erreur email incorrect */
 router.post("/sign-in", async function (req, res, next) {
   var result = false;
   var user = null;
@@ -92,22 +102,23 @@ router.post("/sign-in", async function (req, res, next) {
   res.json({ result, user, error, token });
 });
 
-/*route pour afficher les produits sur la products Screen
-Je vais une recherche du token de l'user, ce qui me permets de voir sil'user est connecté, 
-il faudra stocker dans le store son token lors de sa connection ou inscritpion et le lire
-dans la Producst screen. Si l'user est connecté (donc non null), je fais une recherche des fermes dont le département
-est égal au dép de l'user et je populate avec articles (clé étrangère du model farmer)
-Sinon si l'user n'est pas connecté, il a rentré son code postal dans la page home, il faudra le stocker
-dans le store redux et l'afficher à la page Products screen et l'imposer en condition contraire */
+/*route pour afficher les produits en fonction de la catégorie sur Products Screen
+1er etape: recherche de l'user grâce au token pour voir s'il est connecté ou non 
+Si l'user est connecté (donc non null), recherche des fermes dont le département
+est égal au département de l'user et je populate avec la collection articles (clé étrangère du model farmer en bdd)
+Dans le tableau articles, on copie tous les articles de result
+Sinon si l'user n'est pas connecté, il a rentré son code postal dans la page home. 
+Cette info a été enregistrée dans le store grâce à un reducer.
+On reçoit en back le departement, on fait une recherche des fermes livrant dans le departement de l'user. 
+S'il existe des fermes du même département, on recherche les articles liées à ces fermes et on copie les articles dans
+un tableau nommé articles
+2e etape: On filtre les articles en fonction de leur catégorie pour n'envoyer au front end que les articles de la catégorie choisie */
 router.get("/articles", async function (req, res, next) {
   var articles = [];
   let result = [];
   var user = await UserModel.findOne({ token: req.query.token });
   var cities = await FarmerModel.find({ departement: req.query.departement });
-  var categorieName = await ArticleModel.find({
-    categorie: req.query.categorie,
-  });
-  //test commentaire
+
   if (user != null) {
     result = await FarmerModel.find({
       departement: user.departement,
@@ -120,7 +131,6 @@ router.get("/articles", async function (req, res, next) {
     }
   }
 
-  console.log("tout le req", req.query);
   if (cities != null) {
     result = await FarmerModel.find({
       departement: req.query.departement,
@@ -136,23 +146,25 @@ router.get("/articles", async function (req, res, next) {
     (element) => element.categorie === req.query.categorie
   );
 
-  //console.log("test: ", articlesFilter);
-
   res.json({ articlesFilter, cities, user });
 });
 
+/*Route qui permet d'enregistrer la commande passée et de l'affecter à l'user concerné
+Recherche de l'user en bdd grâce au token
+S'il existe, on enregistre en bdd une commande grâce au modèle user avec les infos reçues depuis le front
+Pour le numéro d'ordre, on génère un numéro aléatoire à 6 chiffres
+pour les articles : étant donné qu'on reçoit une string, on utilise la fonction split pour transformer la chaîne de caractères par un tableau d'obects déparés par une ,
+Si la commande a été enregistrée, on push dans le tableau des ordres de l'user (cf modeèle en bdd) la commande qui vient d'être enregistrée
+Si on a réussi à affecter la commande à l'user on revoit au front le resultat de la réponse et les infos de l'user sinon on renvoit un message d'erreur*/
 router.post("/orders", async function (req, res, next) {
   var result = false;
   var response = false;
   var orderNumber = Math.floor(Math.random() * Math.floor(1000000) + 1);
   var shippingCost = 5;
-  console.log(orderNumber);
   var articlesarray = [];
 
   var user = await UserModel.findOne({ token: req.body.token });
-  // console.log("infouser", user.orders);
   articlesarray = req.body.articles.split(",");
-  console.log("articlesarray", articlesarray);
 
   if (user != null) {
     var newOrder = new OrderModel({
@@ -170,42 +182,46 @@ router.post("/orders", async function (req, res, next) {
     if (orderSave) {
       result = true;
     }
-    console.log("ordersave", orderSave);
+
     //il faut push order dans models
     if (result) {
       user.orders.push(orderSave._id);
       response = true;
-      // console.log("orderscopfinal", user);
+
       await user.save();
       res.json({ user, response });
     } else {
       res.json(err.message);
     }
-    //console.log("answer", response);
-    //console.log("detail", user);
   } else {
     res.json("Something went wrong");
   }
 });
 
-/*recherche dans la bdd des lockers correspondant au departement de l'utilisateur */
+/*recherche dans la bdd des lockers correspondant au departement de l'utilisateur 
+On reçoit depuis le front le token de l'utilisateur. 
+Grâe au token, on recherche en bdd l'user.
+S'il existe, on cherche tous les lockers dont le departement est égal à celui de l'user et on les renvoit en réponse au front*/
 router.get("/lockers", async function (req, res, next) {
   var result = [];
 
   var user = await UserModel.findOne({ token: req.query.token });
-  var data = await LockerModel.find({ departement: req.query.departement });
-  console.log(user);
+
   if (user != null) {
     result = await LockerModel.find({
       departement: user.departement,
     });
   }
-  console.log(result);
 
   res.json({ result });
 });
 
-/*route pour afficher les infos de l'user et ses commandes dans la page account */
+/*route pour afficher les infos de l'user et ses commandes dans la page account
+ Grâce au token reçu depuis le front, on recherche en bdd l'user connecté
+ S'il l'user existe, on stocke ses info dans la variable info
+ On stocke dans le tableau result,  toutes les commandes liés à notre user (order est clé étrangère de la collection user)
+ On copie dans le tableau orders initialisé à vide, tous les orders de l'user
+ On renvoie au front les infos de l'user ainsi que toutes ses commandes*/
 router.get("/account", async function (req, res, next) {
   var orders = [];
   let info;
@@ -223,20 +239,20 @@ router.get("/account", async function (req, res, next) {
       orders = [...orders, ...result[i].orders];
     }
   }
-  console.log("orders", orders);
-  //console.log("info", info[0].username);
 
   res.json({ orders, info });
 });
 
-/*Route pour afficher le détail d'un produit dans la page detail Screen */
+/*Route pour récupérer l'username de l'user
+On reçoit le token depuis le front,
+On recherche dans la bdd s'il existe et si oui ont recupère les infos de l'user grâce au token
+On renvoit au front les infos de l'user  */
 router.get("/user-info", async function (req, res, next) {
   var data = await UserModel.findOne({ token: req.query.token });
 
   if (data != null) {
     detailUser = await UserModel.findOne({ token: req.query.token });
   }
-  console.log(detailUser);
 
   res.json({ detailUser });
 });
